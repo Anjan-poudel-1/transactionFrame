176335bb24a4970e8e8b8bf70115fbf6bd459743
Anjan-Poudel-1 <poudelanjan8@gmail.com>
2024-04-10 14:09:53 +0545

    Transfer dusdc on sepolia - re

--- src/app/api/basic/route.ts
+++ src/app/api/basic/route.ts
@@ -1,40 +1,84 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
+import { FrameRequest, getFrameHtmlResponse } from "@coinbase/onchainkit/frame";
+import { getUserDataForFid } from "frames.js";
+import { getFrameMessage } from "@coinbase/onchainkit";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams
-  const id = searchParams.get('id')
+    const body: FrameRequest = await req.json();
 
-  const idAsNumber = id ? Number(id) : 1
+    const { untrustedData, trustedData } = body; // The data received from frame consist of untrustedData and trustedData.Explore documentation for more,
 
-  const nextId = idAsNumber + 1
+    console.log("body", body);
 
-  if (idAsNumber === 4) {
-    return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame 4</title>
-    <meta property="og:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-4.png" />
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-4.png" />
-    <meta property="fc:frame:button:1" content="View Tutorial" />
-    <meta property="fc:frame:button:1:action" content="link" />
-    <meta property="fc:frame:button:1:target" content="https://github.com/ChangoMan/frames" />
-    <meta property="fc:frame:button:2" content="Restart" />
-    <meta property="fc:frame:button:2:action" content="post" />
-    <meta property="fc:frame:button:2:target" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=1" />
-    </head></html>`)
-  }
+    const { isValid, message } = await getFrameMessage(body, {
+        allowFramegear: true,
+    });
 
-  return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame ${id}</title>
-    <meta property="og:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-${id}.png" />
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-${id}.png" />
-    <meta property="fc:frame:button:1" content="Next Page" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=${nextId}" />
-  </head></html>`)
+    if (!isValid) {
+        return new NextResponse(
+            getFrameHtmlResponse({
+                image: { src: `${process.env.NEXT_PUBLIC_SITE_URL}/error.png` },
+            })
+        );
+    }
+
+    const userDataFromFID = await getUserDataForFid({
+        fid: untrustedData.fid,
+    });
+
+    console.log("userDataFromFID", userDataFromFID);
+
+    //     return new NextResponse(`<!DOCTYPE html><html><head>
+    //     <title>This is frame ${id}</title>
+    //     <meta property="og:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-${id}.png" />
+    //     <meta property="fc:frame" content="vNext" />
+    //     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-${id}.png" />
+    //     <meta property="fc:frame:button:1" content="Next Page" />
+    //     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=${nextId}" />
+    //   </head></html>`);
+
+    let toReturnJSON = {
+        chainId: "eip155:11155111",
+        method: "eth_sendTransaction", //method Id or function selector
+        params: {
+            abi: [
+                {
+                    inputs: [
+                        {
+                            internalType: "address",
+                            name: "recipient",
+                            type: "address",
+                        },
+                        {
+                            internalType: "uint256",
+                            name: "amount",
+                            type: "uint256",
+                        },
+                    ],
+                    name: "transfer",
+                    outputs: [
+                        {
+                            internalType: "bool",
+                            name: "",
+                            type: "bool",
+                        },
+                    ],
+                    stateMutability: "external",
+                    type: "function",
+                },
+            ], // JSON ABI of the function selector and any errors
+            to: "0x381aF7f771E3939dd4037E572D1C5e4b31d4e5Ee",
+            data: "0xa9059cbb000000000000000000000000042cc83d85b4d0f53844036ac5bbccb14ff48b900000000000000000000000000000000000000000000000000000000000a7d8c0",
+            value: "1000000",
+        },
+    };
+
+    console.log("toReturnJSON", toReturnJSON);
+    return new NextResponse(JSON.stringify(toReturnJSON));
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req)
+    return getResponse(req);
 }
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
